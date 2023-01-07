2e885f455953d3e0d57b928ff2acad6deb72459d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-06 19:07:05 -0500

    Event-bus implementation

---
+++ event-bus/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios
+    .post('http://localhost:4000/events', event)
+    .then((res) => console.log(res))
+    .catch((err) =>
+      console.log(`Error sending event to Posts Service: ${err.message}`)
+    );
+  axios
+    .post('http://localhost:4001/events', event)
+    .then((res) => console.log(res))
+    .catch((err) =>
+      console.log(`Error sending event to Comments Service: ${err.message}`)
+    );
+  /* axios
+    .post('http://localhost:4002/events', event)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.message)); */
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Event-bus listening on 4005');
+});
--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -17,7 +18,13 @@ app.get(postsEndpoint, (req, res) => {
 app.post(postsEndpoint, (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
-  posts[id] = { id, title };
+  const post = { id, title };
+  posts[id] = post;
+
+  axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: post,
+  });
 
   res.status(201).send(posts[id]);
 });
